0ce58b4a5c55ba326600241e1d997c97a99a2905
oe.gonzalez@gmail.com <oe.gonzalez@gmail.com>
2022-09-27 19:17:20 +0000

    adding variables and assigment

--- 1. runnning and quitting/1. running and quittint notebook.py
+++ 1. runnning and quitting/1. running and quittint notebook.py
@@ -3,3 +3,21 @@
 # MAGIC 1. uno
 # MAGIC 2. dos
 # MAGIC 3. tres
+
+# COMMAND ----------
+
+age = 42
+first_name = 'Ahmed'
+
+# COMMAND ----------
+
+print(first_name, 'is', age, 'years old')
+
+# COMMAND ----------
+
+age = age + 3
+print(age)
+
+# COMMAND ----------
+
+
