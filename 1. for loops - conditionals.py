2b643a67de81b2e815cf2c8daf53479e4f86c36a
oe.gonzalez@gmail.com <oe.gonzalez@gmail.com>
2022-09-28 19:11:51 +0000

    add for loops conditionals

---
+++ 1. for loops - conditionals.py
@@ -0,0 +1,37 @@
+# Databricks notebook source
+for number in [2, 3, 5]:
+    print(number)
+
+# COMMAND ----------
+
+primes = [2, 3, 5]cumulative = []
+
+for p in primes:
+    squared = p ** 2
+    cubed = p ** 3
+    print(p, squared, cubed)
+
+# COMMAND ----------
+
+total = 0
+for number in range(10):
+   total = total + (number + 1)
+print(total)
+
+# COMMAND ----------
+
+cumulative = []
+total = 0
+data = [1,2,2,5]
+for number in data:
+    cumulative.append(total)
+    total = total + number
+print(cumulative)
+
+
+# COMMAND ----------
+
+masses = [3.54, 2.07, 9.22, 1.86, 1.71]
+for m in masses:
+    if m > 3.0:
+        print(m, 'is large')
