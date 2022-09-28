b7d7e924534ffc17c94d70f0aac87f17ad420add
oe.gonzalez@gmail.com <oe.gonzalez@gmail.com>
2022-09-28 15:07:01 +0000

    add reader cs to dataframe

--- 1. reading tabular data into dataframes.py
+++ 1. reading tabular data into dataframes.py
@@ -1,2 +1,41 @@
 # Databricks notebook source
+# MAGIC %fs
 
+# COMMAND ----------
+
+dbutils.fs.pwd()
+
+# COMMAND ----------
+
+import pandas as pd
+
+data = pd.read_csv("/dbfs/mnt/data/gapminder_gdp_oceania.csv")
+
+print(data)
+
+# COMMAND ----------
+
+mount_name = "data"
+display(dbutils.fs.ls(f"/mnt/data"))
+
+# COMMAND ----------
+
+print(data.columns)
+
+# COMMAND ----------
+
+print(data.T)
+
+# COMMAND ----------
+
+data.describe()
+      
+
+# COMMAND ----------
+
+americas = pd.read_csv('/dbfs/mnt/data/gapminder_gdp_americas.csv', index_col='country')
+americas.describe()
+
+# COMMAND ----------
+
+americas_flipped.tail(n=3)
